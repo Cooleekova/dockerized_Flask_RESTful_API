9d343c61f92c2b32d86c559316793f13792608fe
Atya <nushnushka@gmail.com>
2021-06-27 21:03:26 +0300

    Revert "-"
    This reverts commit fb9794fd

--- app/views.py
+++ app/views.py
@@ -4,7 +4,9 @@ from app import app
 from app.validator import validate
 from app.models import Ad, User
 from app.schema import USER_CREATE
-from app import db
+
+# Должны быть реализованы методы создания/удаления/редактирования объявления
+# POST метод должен создавать объявление, GET - получать объявление, DELETE - удалять объявление.
 
 
 class UserView(MethodView):
@@ -23,26 +25,19 @@ class UserView(MethodView):
 
 class AdView(MethodView):
 
-    def get_ad(self, ad_id):
+    def get(self, ad_id):
         ad = Ad.by_id(ad_id)
         return jsonify(ad.to_dict())
 
-    def post_ad(self):
+    def post(self):
         ad = Ad(**request.json)
         ad.add()
         return jsonify(ad.to_dict())
 
-    def delete_ad(self, ad_id):
+    def delete(self, ad_id):
         ad = Ad.by_id(ad_id)
         ad.delete()
-        return jsonify({'message': 'Ad was deleted'})
-
-    def update_ad(self, ad_id):
-        ad = Ad.by_id(ad_id)
-        ad.title = request.json['title']
-        ad.description = request.json['description']
-        db.session.commit()
-        return jsonify({'message': 'Ad was updated'})
+        return jsonify({'message': f'Ad was deleted'})
 
 
 app.add_url_rule('/users/<int:user_id>', view_func=UserView.as_view('users_get'), methods=['GET', ])
@@ -51,4 +46,3 @@ app.add_url_rule('/users/', view_func=UserView.as_view('users_create'), methods=
 app.add_url_rule('/ads/', view_func=AdView.as_view('ads_create'), methods=['POST', ])
 app.add_url_rule('/ads/<int:ad_id>', view_func=AdView.as_view('ads_get'), methods=['GET', ])
 app.add_url_rule('/ads/<int:ad_id>', view_func=AdView.as_view('ads_delete'), methods=['DELETE', ])
-app.add_url_rule('/ads/<int:ad_id>', view_func=AdView.as_view('ads_update'), methods=['PATCH', ])
