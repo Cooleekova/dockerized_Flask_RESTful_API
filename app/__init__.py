544e7a063eb7f28f570da337e61da91bd1e5c993
cooleekova <nushnushka@gmail.com>
2022-10-17 15:19:19 -0300

    Change environmental variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from app import config
-from module_dotenv import secret_key
 
 
 app = Flask(__name__)
 app.config.from_mapping(SQLALCHEMY_DATABASE_URI=config.POSTGRES_URI)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = secret_key
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 from app import views
+from .models import User, Ad
+
+db.create_all()
+db.session.commit()
--- app/config.py
+++ app/config.py
@@ -1,4 +1,11 @@
-from module_dotenv import user, password, host, port, db
+import os
 
-POSTGRES_URI = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+user = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
+host = os.environ.get('DB_HOST')
+port = os.environ.get('DB_PORT')
+database = os.environ.get('DATABASE')
+
+
+POSTGRES_URI = f'postgresql://{user}:{password}@{host}:{port}/{database}'
 SALT = 'my_sJHLHLHKL_)jzekehckiuper_s!alt_#4$4344'
